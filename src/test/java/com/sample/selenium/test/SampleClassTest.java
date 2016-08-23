9a487dec9343c3abca1bcf279c3892acef269b4a
Narayana <chinniz999@gmail.com>
2016-08-23 08:12:35 +0530

    Added testng.xml Modified pom.xml added test cases in class.

---
+++ src/test/java/com/sample/selenium/test/SampleClassTest.java
@@ -0,0 +1,17 @@
+package com.sample.selenium.test;
+
+import org.testng.annotations.Test;
+
+public class SampleClassTest{
+	@Test
+	public  void sample() {
+		System.out.println("Testing");
+	}
+	
+	
+	@Test
+	public  void sample02() {
+		System.out.println("Testing");
+	}
+
+}
