93c69f69dab77cca341384957d00a4930b9934e1
Narayana <chinniz999@gmail.com>
2016-08-27 18:59:38 +0530

    Report ng Itextcontext depenency in pom for reportn ng

--- src/main/java/com/sample/selenium/constants/Setup.java
+++ src/main/java/com/sample/selenium/constants/Setup.java
@@ -3,5 +3,7 @@ package com.sample.selenium.constants;
 public interface Setup {
 
 	public static final String JSONFILE="D:\\JenkinsSample\\JenkinsSample\\src\\test\\resources\\TestData\\PersonalDetails.json";
+	
+	public static final String sAdding="Add";
 											
 }
--- src/test/java/com/sample/selenium/test/SampleClassTest.java
+++ src/test/java/com/sample/selenium/test/SampleClassTest.java
@@ -1,17 +1,47 @@
 package com.sample.selenium.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class SampleClassTest{
-	@Test
-	public  void sample() {
-		System.out.println("Testing");
+public class SampleClassTest {
+
+	// testng - Testing Framework
+	// Annotation
+	WebDriver driver = null;
+
+	@BeforeMethod
+	public void beforeMethod() {
+		driver = new FirefoxDriver();
+		System.out.println("--------------" + "Before Method");
+		driver.get("http://www.google.com");
 	}
-	
-	
+
+	@AfterMethod
+	public void AfterMethod() {
+		System.out.println("--------------" + "@AfterMethod");
+		driver.quit();
+	}
+
 	@Test
-	public  void sample02() {
-		System.out.println("Testing");
+	public void test1() {
+		System.out.println("testing-01" + driver.getTitle());
+	}
+
+	@Test
+	public void test2(ITestContext description) {
+		System.out.println("testing-02" + driver.getTitle());
+		description.getAttribute("AA");
+		
+	}
+
+	@Test(description = "Verify Page title")
+	public void test3() {
+		System.out.println("testing-03" + driver.getTitle());
 	}
 
 }
